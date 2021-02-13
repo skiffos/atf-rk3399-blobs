a252d265f94d37287123a08812b27186e1fa3e0a
Christian Stewart <christian@paral.in>
2021-02-13 04:06:02 -0800

    u-boot: use separate per-target versions
    Signed-off-by: Christian Stewart <christian@paral.in>

--- u-boot.bash
+++ u-boot.bash
@@ -10,12 +10,13 @@ if [ -z $BOARD ]; then
     BOARD=pinebook-pro
 fi
 
+pushd ./${BOARD}
 make -C ./u-boot ${BOARD}-rk3399_defconfig
 make -C ./u-boot \
      -j8 PLAT=rk3399 \
-     BL31=../bl31.elf \
+     BL31=../../bl31.elf \
      CFLAGS='-gdwarf-2' \
      M0_CROSS_COMPILE=arm-none-eabi- \
      CROSS_COMPILE=aarch64-unknown-linux-gnu- \
      u-boot.itb idbloader.img
-cp ./u-boot/u-boot.itb ./u-boot/idbloader.img ./${BOARD}/
+cp ./u-boot/u-boot.itb ./u-boot/idbloader.img ./
